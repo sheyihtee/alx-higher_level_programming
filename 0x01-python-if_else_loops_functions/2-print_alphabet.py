720e21748ef35f2bdc00e22e198197dde8af89c1
sheyihtee <sheyihtee@gmail.com>
2023-06-06 13:39:40 -0700

    abcdef..

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end='')
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
